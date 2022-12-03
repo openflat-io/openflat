2936cbc1ad529c1eca651b50ff9a919c5a625e7e
qianzhu <36325249+syt-honey@users.noreply.github.com>
2022-12-03 15:40:16 +0800

    refactor(ci): update cspell config

--- web/flat-web/dist/assets/lifecycle.d6023d27.js
+++
@@ -1,2 +0,0 @@
-import{l as s}from"./index.003b8e05.js";function f(){const e=s.exports.useRef(!1);return s.exports.useEffect(()=>()=>{e.current=!0},[]),e}function i(){const e=f();function n(o,t){return new Promise(async(u,c)=>{try{const r=await o;e.current||u(r)}catch(r){e.current?t?t(r):console.error("An error occurs from a promise after a component is unmounted",r):c(r)}})}return s.exports.useCallback(n,[e])}export{i as u};
-//# sourceMappingURL=lifecycle.d6023d27.js.map
